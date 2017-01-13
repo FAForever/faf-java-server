8c948ec9300ba1bb22c739671b38bf7c3ffdc799
Michel Jung <michel.jung89@gmail.com>
2017-01-13 15:11:55 +0100

    Update code formatting and arrangement

--- src/main/java/com/faforever/server/entity/Game.java
+++ src/main/java/com/faforever/server/entity/Game.java
@@ -7,10 +7,23 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Transient;
 import java.sql.Timestamp;
 import java.time.Instant;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @Entity
@@ -26,27 +39,35 @@ public class Game {
    */
   @Transient
   private final Map<String, Object> options;
+
   @Transient
   private final AtomicInteger desyncCounter;
+
   /**
    * Maps player IDs to army scores reported by this player.
    */
   @Transient
   private final Map<Integer, List<ArmyScore>> reportedArmyScores;
+
   /**
    * Maps player IDs to army outcomes reported by this player.
    */
   @Transient
   private final Map<Integer, List<ArmyOutcome>> reportedArmyOutcomes;
+
   @Transient
   private final List<ArmyStatistics> armyStatistics;
+
   @Transient
   private final Map<Integer, Player> activePlayers;
+
   @Id
   @Column(name = "id")
   private int id;
+
   @Column(name = "startTime")
   private Timestamp startTime;
+
   @Column(name = "gameType")
   @Enumerated(EnumType.ORDINAL)
   private VictoryCondition victoryCondition;
@@ -56,49 +77,65 @@ public class Game {
   @JoinColumn(name = "gameMod")
   @ManyToOne
   private FeaturedMod featuredMod;
+
   @ManyToOne
   @JoinColumn(name = "host")
   private Player host;
+
   @ManyToOne
   @JoinColumn(name = "mapId")
   private MapVersion map;
+
   @Column(name = "gameName")
   private String title;
+
   @Column(name = "validity")
   @Enumerated(EnumType.ORDINAL)
   private Rankiness rankiness;
+
   @OneToMany(mappedBy = "game")
   private List<GamePlayerStats> playerStats;
+
   @Transient
   private GameState state = GameState.INITIALIZING;
+
   @Transient
   private String password;
+
   /**
    * Since some maps are unknown by the server (e.g. in-develop versions from map creators), the literal map name
    * is kept.
    */
   @Transient
   private String mapName;
+
   /**
    * Maps player IDs to key-value option maps, like {@code 1234 -> "Color" -> 1 }
    */
   @Transient
   private Map<Integer, Map<String, Object>> playerOptions;
+
   /**
    * Maps AI names to key-value option maps, like {@code "AI: Rufus" -> "Color" -> 1 }
    */
   @Transient
   private Map<String, Map<String, Object>> aiOptions;
+
   @Transient
   private int maxPlayers;
+
   @Transient
   private List<String> simMods;
+
   @Transient
   private boolean ratingEnforced;
+
   @Transient
   private GameVisibility gameVisibility;
+
   @Transient
   private Instant launchedAt;
+
   @Transient
   private boolean mutuallyAgreedDraw;
 
