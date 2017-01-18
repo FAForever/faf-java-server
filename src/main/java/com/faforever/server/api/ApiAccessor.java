703c497d66a690c76d91b66209fe226d73af29a9
Michel Jung <michel.jung89@gmail.com>
2017-01-18 03:28:16 +0100

    Implement achievements & events call to API

--- src/main/java/com/faforever/server/api/ApiAccessor.java
+++ src/main/java/com/faforever/server/api/ApiAccessor.java
@@ -3,9 +3,11 @@ package com.faforever.server.api;
 import com.faforever.server.api.dto.UpdatedAchievement;
 import com.faforever.server.config.FafServerProperties;
 import com.faforever.server.stats.achievements.AchievementUpdate;
+import com.faforever.server.stats.event.EventUpdate;
 import org.springframework.security.oauth2.client.OAuth2RestOperations;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.CompletionStage;
 
@@ -25,6 +27,10 @@ public class ApiAccessor {
 
   public CompletionStage<List<UpdatedAchievement>> updateAchievements(List<AchievementUpdate> achievementUpdates) {
     return supplyAsync(() -> asList(restOperations.patchForObject(url("/player_achievements"), achievementUpdates, UpdatedAchievement[].class)));
+  }
+
+  public void updateEvents(List<EventUpdate> eventUpdates) {
+    supplyAsync(() -> Collections.singletonList(restOperations.patchForObject(url("/player_events"), eventUpdates, Void.class)));
   }
 
   private String url(String route) {
