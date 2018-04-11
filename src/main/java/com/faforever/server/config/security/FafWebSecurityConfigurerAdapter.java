2d66116a421745c2693a43ca5dfc4465e2a4c1fe
Michel Jung <michel.jung89@gmail.com>
2018-04-12 01:06:29 +0200

    Fix security config
    Removes global method security config since it has not been used
    (channel security is used instead).

--- src/main/java/com/faforever/server/config/security/FafWebSecurityConfigurerAdapter.java
+++ src/main/java/com/faforever/server/config/security/FafWebSecurityConfigurerAdapter.java
@@ -1,16 +1,46 @@
 package com.faforever.server.config.security;
 
+import com.google.common.collect.ImmutableMap;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Map;
 
 @Configuration
 public class FafWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+
+  private final UserDetailsService userDetailsService;
+
+  public FafWebSecurityConfigurerAdapter(UserDetailsService userDetailsService) {
+    this.userDetailsService = userDetailsService;
+  }
 
   @Bean
   @Override
   public AuthenticationManager authenticationManagerBean() throws Exception {
     return super.authenticationManagerBean();
+  }
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    String idForEncode = "bcrypt";
+    Map<String, PasswordEncoder> encoders = ImmutableMap.of(
+      idForEncode, new BCryptPasswordEncoder()
+    );
+
+    DelegatingPasswordEncoder passwordEncoder = new DelegatingPasswordEncoder(idForEncode, encoders);
+    passwordEncoder.setDefaultPasswordEncoderForMatches(NoOpPasswordEncoder.getInstance());
+
+    auth
+      .userDetailsService(userDetailsService)
+      .passwordEncoder(passwordEncoder);
   }
 }
