dfba67f54fe71c84b48a676996a2ba33316450e0
Christian Schmidt <c-schmidt@gmx.eu>
2018-05-21 01:14:54 +0200

    Add NoArgsConstructor to all v2-client-messages

--- src/main/java/com/faforever/server/integration/v2/client/ArmyStatsClientMessage.java
+++ src/main/java/com/faforever/server/integration/v2/client/ArmyStatsClientMessage.java
@@ -2,12 +2,14 @@ package com.faforever.server.integration.v2.client;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 /**
  * Message sent from the client to the server informing it about army statistics.
  */
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 class ArmyStatsClientMessage extends V2ClientMessage {
   private String stats;
 }
--- src/main/java/com/faforever/server/integration/v2/client/DisconnectClientClientMessage.java
+++ src/main/java/com/faforever/server/integration/v2/client/DisconnectClientClientMessage.java
@@ -2,12 +2,14 @@ package com.faforever.server.integration.v2.client;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 /**
  * Message sent from the client to the server to request disconnecting a client from the server.
  */
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 class DisconnectClientClientMessage extends V2ClientMessage {
   private int userId;
 }
