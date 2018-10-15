06b58bb64852d4aedb6b3d6c7d6d34a0d9810959
Michel Jung <michel.jung89@gmail.com>
2018-10-15 23:31:17 +0200

    Rename to HttpSecurityConfig and fix order
    The existing web security config is order 100

--- faf-java-server-app/src/main/java/com/faforever/server/config/security/HttpSecurityConfig.java
+++ faf-java-server-app/src/main/java/com/faforever/server/config/security/HttpSecurityConfig.java
@@ -2,11 +2,13 @@ package com.faforever.server.config.security;
 
 import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-public class EndpointWebSecurityConfig extends WebSecurityConfigurerAdapter {
+@Order(101)
+public class HttpSecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
