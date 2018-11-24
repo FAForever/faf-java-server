da778f0e2399b22bb329b1f2c4663c325549fe45
Michel Jung <michel.jung89@gmail.com>
2018-11-24 10:43:30 +0100

    Improve JavaDoc of `ClientMessage` and `ServerMessage`

--- faf-java-server-app/src/main/java/com/faforever/server/common/ClientMessage.java
+++ faf-java-server-app/src/main/java/com/faforever/server/common/ClientMessage.java
@@ -1,7 +1,14 @@
 package com.faforever.server.common;
 
 /**
- * Tag interface for messages sent from the client to the server.
+ * Tag interface to be implemented by classes who internally represent a message sent from the client to the server.
+ * Depending on the nature of the message, subclasses should be suffixed with either {@code Request} (the message
+ * expects a response from the server) or {@code Report} (the message is fire-and-forget).
+ * <p>
+ * Subclasses are for server-internal use only; they are not part of the client-server contract and, therefore, must not
+ * be deserialized directly and their name and fields may change at any time.
+ *
+ * @see ServerMessage
  */
 public interface ClientMessage {
 
--- faf-java-server-app/src/main/java/com/faforever/server/common/ServerMessage.java
+++ faf-java-server-app/src/main/java/com/faforever/server/common/ServerMessage.java
@@ -1,5 +1,15 @@
 package com.faforever.server.common;
 
+/**
+ * Tag interface to be implemented by classes who internally represent a message sent from the server to the client.
+ * Depending on the nature of the message, subclasses should be suffixed with either {@code Response} (the message is
+ * triggered by a client request) or {@code Message} (the message is fire-and-forget).
+ * <p>
+ * Subclasses are for server-internal use only; they are not part of the client-server contract and, therefore, must not
+ * be serialized directly and their name and fields may change at any time.
+ *
+ * @see ClientMessage
+ */
 public interface ServerMessage {
 
 }
